3d0d09506f792f538771e8778ac30c9e841fff4b
Mircea Milencianu <mircea.milencianu@gmail.com>
2020-07-27 22:01:03 +0300

    random runner

--- main.sh
+++ main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Running script..."
-python /srv/pyCode/infinite-strategies-env/src/infinite_strategies/round_robin.py -r
-echo "Done..."
+RUNS=$1
+WAIT=$((1 + RANDOM % 60))
+for i in {1..$RUNS}; 
+do
+    echo "Running script in $WAIT seconds."
+    sleep $WAIT
+    python /srv/pyCode/infinite-strategies-env/src/infinite_strategies/round_robin.py -r
+    echo "Done";
+done
