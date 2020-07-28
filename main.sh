44e6a06ee2e0a08851b91df44797e250d4669d5a
Mircea Milencianu <mircea.milencianu@gmail.com>
2020-07-28 16:27:30 +0300

    working changes

--- main.sh
+++ main.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 RUNS=$1
-WAIT=$((1 + RANDOM % 60))
+# WAIT=$((1 + RANDOM % 60))
 for i in {1..$RUNS}; 
 do
-    echo "Running script in $WAIT seconds."
-    sleep $WAIT
+    echo "Running script."
+    # sleep $WAIT
     python /srv/pyCode/infinite-strategies-env/src/infinite_strategies/round_robin.py -r TRUE
     echo "Done";
 done
