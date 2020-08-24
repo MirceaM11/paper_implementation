0a759e28cd77ce70241c72d37c7f437cb893e70b
Mircea Milencianu <mircea.milencianu@gmail.com>
2020-08-24 10:48:14 +0300

    fix error

--- main.sh
+++ main.sh
@@ -6,6 +6,6 @@ do
     WAIT=$((1 + RANDOM % 10))
     echo "Running script. Run number: ${i} with wait time: ${WAIT}"
     sleep $WAIT
-    python /srv/pyCode/infinite-strategies-env/src/infinite_strategies/round_robin.py -r TRUE
+    python infinite-strategies-env/src/infinite_strategies/round_robin.py -r TRUE
     echo "Done";
 done
